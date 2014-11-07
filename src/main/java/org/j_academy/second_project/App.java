09d405973413bf8ffa764277e0af4b556df12686
Ahmed Bacha <ahmedbachaabdelkrim@gmail.com>
2014-11-07 15:18:30 +0100

    ADD profile

--- src/main/java/org/j_academy/second_project/App.java
+++ src/main/java/org/j_academy/second_project/App.java
@@ -3,6 +3,7 @@ package org.j_academy.second_project;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 
 /**
  * Hello world!
@@ -12,14 +13,13 @@ public class App
 {
 	private static Logger logger = LogManager.getLogger(App.class);
 	
-	
     public static void main( String[] args )
     {
         logger.info( "Hello World!" );
         
         DateTime now = new DateTime();
         
-        logger.info( "Temps acctuel : "+now );
+        logger.info( "Temps acctuel : "+ now );
         
         logger.info( "Temps heure : " + now.getHourOfDay() );
         
